ec9959c5f7767c59253024255d94e1f0e6e45111
xmaihh <xmaihh@protonmail.com>
2022-03-30 11:34:36 +0800

    [loon] <script> Fix v2ex.js

--- loon/script/v2ex/v2ex.cookie.js
+++ loon/script/v2ex/v2ex.cookie.js
@@ -1,12 +1,12 @@
 const cookieName = 'V2EX'
-const cookieKey = 'cookie_v2ex'
+const cookieKey = 'chavy_cookie_v2ex'
 const cookieVal = $request.headers['Cookie']
 
 if (cookieVal) {
-  let cookie = $prefs.setValueForKey(cookieVal, cookieKey)
+  let cookie = $persistentStore.write(cookieVal, cookieKey)
   if (cookie) {
     let msg = `${cookieName}`
-    $notify(msg, 'Cookie写入成功', '详见日志')
+    $notification.post(msg, 'Cookie写入成功', '详见日志')
     console.log(msg)
     console.log(cookieVal)
   }
